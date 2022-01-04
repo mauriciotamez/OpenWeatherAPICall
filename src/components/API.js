a694959f7c30eb1d88e212688e211fd1eab1b069
Mauricio Tamez <vitxlssreturn@gmail.com>
2022-01-03 20:28:31 -0600

    Recovered lost code and

--- src/components/API.js
+++ src/components/API.js
@@ -1,30 +1,23 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-
+import axios from "axios";
+import { useEffect, useState } from "react";
+import moment from "moment";
 
 const API = () => {
 
+    // useStates  ----------------------------------------------------------------------------------------------------------------------------------------------
     const [data, setData] = useState(null);
-    const [button, setButton] = useState();
+    const [button, setButton] = useState(true);
     
-    
-    // Call OpenWeather API
-    
+    // Get user coords to later use on the endpoint four our request to te API via AXIOS -----------------------------------------------------------------------
     useEffect(() => {
 
-        const error = () => {
-            alert("Sorry, no position available.")
-        };
-            
-        const options = {
-            enableHighAccuracy: true,
-            maximumAge: 30000,
-            timeout: 27000
-        };
+        const error = () => alert("Sorry, you need to allow Geolocation in order to use this website.");
+   
+        const options = { enableHighAccuracy: false, maximumAge: 30000, timeout: 27000 };
         
         const success = (position) => {
-            const lat = position.coords.latitude;
-            const lon = position.coords.longitude;   
+            const lat = position.coords.latitude.toFixed(2);
+            const lon = position.coords.longitude.toFixed(2);   
             axios
             .get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=efb990a58f6c2110b7599bb221cc00ae`)
             .then(res => {
@@ -37,43 +30,72 @@ const API = () => {
 
     }, []);
     
+    //  Functions for our buttons to execute in the onClicks----------------------------------------------------------------------------------------------------
+    const btnStateFn = () => setButton(!button);
     
-    const btnFunction = () => {
-        setButton(kelvinToFahrenheit(num))
-    }
+    const reloadIconFN = () => window.location.reload();
     
     // Convert Kelvin to Celsius
-
-    const kelvinToCelsius = (num) =>  num = parseFloat(num-273.15).toFixed(1);
-    
+    const kelvinToCelsius = (data) =>  data = parseFloat(data-273.15).toFixed(1);
 
     // Convert Kelvin to Fahrenheit
-
-    const kelvinToFahrenheit = (num) => num = parseFloat((num-273.15)*1.8.toFixed(1))+32;
-
-    let num = data?.main.temp
+    const kelvinToFahrenheit = (data) => data = parseFloat((data-273.15)*1.8+32).toFixed(1);
+   
+    // Declare the data that we are going to use as parameters on the functions that convert Kelvin to other units  --------------------------------------------
+   let mainTemp = data?.main.temp
+   let minTemp =  data?.main.temp_min
+   let maxTemp = data?.main.temp_max
     
-    // JSX
-
+    // JSX -----------------------------------------------------------------------------------------------------------------------------------------------------
     return (
-        <div className='weatherCard'>
-            <h1>{data?.name}, {data?.sys.country}</h1>
-            <div className='data'>
+        <div className="weatherCard">
+            <h1>{data?.name},{data?.sys.country}</h1>
+            <div className="data">
                 <ul>
-                    <i class="fas fa-cloud"></i>
-                    <i class="fas fa-wind"></i>
-                    <i class="fas fa-tint"></i>
-                    <li><span className='des1'>{data?.weather[0].description}</span></li>
-                    <li><span className='des2'>Wind Speed is {data?.wind.speed}<span className='lower'> m/s </span></span></li>
-                    <li><span className='des3'>Humidity is {data?.main.humidity}</span></li>
+                    <i className="fas fa-cloud"></i>
+                    <i className="fas fa-wind"></i>
+                    <i className="fas fa-tint"></i>
+                    <li>
+                        <span className="des1">{data?.weather[0].description}</span>
+                    </li>
+                    <li>
+                        <span className="des2">Wind Speed is {data?.wind.speed}
+                            <span className="lower"> m/s </span>
+                        </span>
+                    </li>
+                    <li>
+                        <span className="des3">Humidity : {data?.main.humidity} %</span>
+                    </li>
                 </ul>
-                <button className='btn' onClick={btnFunction}>°C to °F</button>
-                <span className='temp'>{button}{kelvinToCelsius(num)}<span className='grades'>°C</span></span>
+                <div className="minMaxTemp">
+                    <span>Min temp is {
+                        button 
+                        ? kelvinToCelsius(minTemp) 
+                        : kelvinToFahrenheit(minTemp)
+                    } 
+                    <span className="grades">{button ? "C°" : "F°"}</span> /           
+                    </span>
+                    <span>Max temp is {
+                        button 
+                        ? kelvinToCelsius(maxTemp) 
+                        : kelvinToFahrenheit(maxTemp)
+                    } 
+                        <span className="grades">{button ? "C°" : "F°"} </span> 
+                    </span>
+                </div> 
+                <p className="date">{moment().format("dddd")} {moment().format("LL")}</p>
+                <button className="btn" onClick={btnStateFn}> Metric C° / Imperial F°</button>
+                <span className="temp">
+                    {
+                        button 
+                        ? kelvinToCelsius(mainTemp) 
+                        : kelvinToFahrenheit(mainTemp)
+                    }
+                    <span className="grades">{button ? "C°" : "F°"}</span>
+                </span>
+                <button onClick={reloadIconFN} className="btn2"> <i className="fas fa-sync-alt"></i> </button>
                 <img src={`https://openweathermap.org/img/wn/${data?.weather[0]?.icon}@2x.png`} alt="" />
-                
             </div>
-            
-            
         </div>
     );
 };
